07b6912c1f7f62a954d17cd5ca7f8eac78489800
yigitkemal <40325505+yigitkemal@users.noreply.github.com>
2021-11-21 14:43:02 +0300

    Products are added.

--- lib/ProductListTab.dart
+++ lib/ProductListTab.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
+import 'package:shopping_app/ProductRowItem.dart';
 import 'package:shopping_app/model/app_state_model.dart';
 
 class ProductListTab extends StatelessWidget {
+  const ProductListTab({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AppStateModel>(
       builder: (context, model, child) {
+        final products = model.getProducts();
         return CustomScrollView(
-          slivers: [
-            CupertinoSliverNavigationBar(
-              largeTitle: Text('Store'),
-            )
+          semanticChildCount: products.length,
+          slivers: <Widget>[
+            const CupertinoSliverNavigationBar(
+              largeTitle: Text('Cupertino Store'),
+            ),
+            SliverSafeArea(   // BEGINNING OF NEW CONTENT
+              top: false,
+              minimum: const EdgeInsets.only(top: 8),
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                    if (index < products.length) {
+                      return ProductRowItem(
+                        product: products[index],
+                        lastItem: index == products.length - 1,
+                      );
+                    }
+
+                    return null;
+                  },
+                ),
+              ),
+            )     // END OF NEW CONTENT
           ],
         );
       },
